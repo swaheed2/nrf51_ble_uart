4ca965faf29159e539a01f7a2fdd2a80d4b6cd41
Sumama Waheed <sumamawaheed1@gmail.com>
2016-04-24 15:21:15 -0500

    removed unused methods

--- stress_algo.h
+++ stress_algo.h
@@ -1,35 +1,40 @@
+/*
+ * stress_algo.h
+ *
+ *  Created on: Apr 24, 2016
+ *      Author: ubuntu
+ */
+
 #ifndef __STRESS_ALGO_H
 #define __STRESS_ALGO_H
- 
-#define REAL double 
+
+#define REAL double
 #define TOTAL_VALUES 1001
 
 
 
 
 //extern double rrReadings[TOTAL_VALUES];
-extern double STD2;
+extern double SD2;
 
 
 inline static REAL sqr(REAL x);
 
 double getHighestValue(int startIndex, int endIndex);
 void fillAverageArray();
- 
+
 void peakDetect(int count);
 double * getRRValuesArr();
 
 double getRMSSD(double rrReadings[]);
-double getSTD1(double rrReadingsp[]);
-double getSTD2(double rrReadingsp[]);
+double getSD1(double rrReadingsp[]);
+double getSD2(double rrReadingsp[]);
 double getPNN50(double rrReadings[]);
 
 
 double getStressScale(double reading,double x,double y);
 
 
-int linreg(int n, const REAL x[], const REAL y[], REAL* m, REAL* b, REAL* r);
 void printArray(double array[], int size);
-double getBestFitLine(double rrReadings[]); 
- 
+
 #endif
